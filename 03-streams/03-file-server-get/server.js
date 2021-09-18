4ae4ac96e4fe97888bc8166e902851cb771e460f
Anton Merežko <anton.merezko@pipedrive.com>
2021-09-18 14:15:54 +0300

    03-streams/03-file-server-get - solution

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,17 +1,37 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
+  const pathDepth = pathname.split(path.sep).length;
   const filepath = path.join(__dirname, 'files', pathname);
+
+
+  if (pathDepth > 1) {
+    res.statusCode = 400;
+    res.end('Bad request');
+    return;
+  }
 
   switch (req.method) {
     case 'GET':
+      const readStream = fs.createReadStream(filepath);
+      readStream.pipe(res);
+
+      readStream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File not found');
+          return;
+        }
+        res.statusCode = 500;
+        res.end('Internal server error');
+      });
 
       break;
 
