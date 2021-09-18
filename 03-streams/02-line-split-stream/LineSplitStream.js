1e1e883e44a6cc134566f023ef9f00f0eb587e74
Anton Merežko <anton.merezko@pipedrive.com>
2021-09-18 14:00:02 +0300

    03-streams/02-line-split-stream  - solution

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.cache = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const text = this.cache + chunk.toString('utf-8');
+
+    if (text.includes(os.EOL)) {
+      const lines = text.split(os.EOL);
+      this.cache = lines.pop();
+
+      lines.forEach((line) => this.push(line));
+    } else {
+      this.cache = text;
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.cache) {
+      this.push(this.cache);
+    }
+    callback();
   }
 }
 
