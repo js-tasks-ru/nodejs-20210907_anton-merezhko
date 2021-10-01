b3223ae3783182f80ecb52bb1019b19e318a4a09
Anton Merežko <anton.merezko@pipedrive.com>
2021-10-01 12:25:46 +0300

    04-testing-configuration-logging/01-unit-tests  - solution

--- 04-testing-configuration-logging/01-unit-tests/Validator.js
+++ 04-testing-configuration-logging/01-unit-tests/Validator.js
@@ -5,35 +5,51 @@ module.exports = class Validator {
 
   validate(obj) {
     const errors = [];
+    const ruleFields = Object.keys(this.rules);
 
-    for (const field of Object.keys(this.rules)) {
-      const rules = this.rules[field];
+    for (const field of ruleFields) {
+      const toValidateObjectFields = Object.keys(obj);
 
-      const value = obj[field];
-      const type = typeof value;
+      if (toValidateObjectFields.includes(field)) {
+        const rules = this.rules[field];
+        const value = obj[field];
+        const type = typeof value;
 
-      if (type !== rules.type) {
-        errors.push({field, error: `expect ${rules.type}, got ${type}`});
-        return errors;
-      }
+        if (type !== rules.type) {
+          errors.push({field, error: `expect ${rules.type}, got ${type}`});
+          return errors;
+        }
 
-      switch (type) {
-        case 'string':
-          if (value.length < rules.min) {
-            errors.push({field, error: `too short, expect ${rules.min}, got ${value.length}`});
-          }
-          if (value.length > rules.max) {
-            errors.push({field, error: `too long, expect ${rules.max}, got ${value.length}`});
-          }
-          break;
-        case 'number':
-          if (value < rules.min) {
-            errors.push({field, error: `too little, expect ${rules.min}, got ${value}`});
-          }
-          if (value > rules.max) {
-            errors.push({field, error: `too big, expect ${rules.min}, got ${value}`});
-          }
-          break;
+        switch (type) {
+          case 'string':
+            if (value.length < rules.min) {
+              errors.push({
+                field,
+                error: `too short, expect ${rules.min}, got ${value.length}`,
+              });
+            }
+            if (value.length > rules.max) {
+              errors.push({
+                field,
+                error: `too long, expect ${rules.max}, got ${value.length}`,
+              });
+            }
+            break;
+          case 'number':
+            if (value < rules.min) {
+              errors.push({
+                field,
+                error: `too little, expect ${rules.min}, got ${value}`,
+              });
+            }
+            if (value > rules.max) {
+              errors.push({
+                field,
+                error: `too big, expect ${rules.min}, got ${value}`,
+              });
+            }
+            break;
+        }
       }
     }
 
--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -1,22 +1,76 @@
 const Validator = require('../Validator');
 const expect = require('chai').expect;
+const validatorRules = require('./validatorRules.json');
 
 describe('testing-configuration-logging/unit-tests', () => {
   describe('Validator', () => {
-    it('валидатор проверяет строковые поля', () => {
-      const validator = new Validator({
-        name: {
-          type: 'string',
-          min: 10,
-          max: 20,
-        },
-      });
+    before(() => {
+      this.validator = new Validator(validatorRules);
+    });
 
-      const errors = validator.validate({ name: 'Lalala' });
+    it('Validator reports short string value', () => {
+      const errors = this.validator.validate({name: 'Lalala'});
 
       expect(errors).to.have.length(1);
       expect(errors[0]).to.have.property('field').and.to.be.equal('name');
-      expect(errors[0]).to.have.property('error').and.to.be.equal('too short, expect 10, got 6');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('too short, expect 10, got 6');
+    });
+    it('Validator reports long string value', () => {
+      const errors = this.validator.validate({
+        name: 'LalalasdasdasdasdasaLalalasdasdasdasdasaLalalasdasdasdasdasa',
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('too long, expect 20, got 60');
+    });
+    it('Validator reports if not string passed to string field', () => {
+      const errors = this.validator.validate({
+        name: 123123,
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('name');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('expect string, got number');
+    });
+    it('Validator reports short number value', () => {
+      const errors = this.validator.validate({
+        age: 1,
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('too little, expect 18, got 1');
+    });
+    it('Validator reports long number value', () => {
+      const errors = this.validator.validate({
+        age: 101,
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('too big, expect 18, got 101');
+    });
+    it('Validator reports if not number passed to number field', () => {
+      const errors = this.validator.validate({
+        age: '123',
+      });
+
+      expect(errors).to.have.length(1);
+      expect(errors[0]).to.have.property('field').and.to.be.equal('age');
+      expect(errors[0])
+          .to.have.property('error')
+          .and.to.be.equal('expect number, got string');
     });
   });
 });
