4d667bf2fe1cac24d10d9ffc2fd325bcf842041c
Anton Merežko <anton.merezko@pipedrive.com>
2021-10-04 14:11:40 +0300

    Revert "04-testing-configuration-logging/01-unit-tests - add more tests"
    This reverts commit bde2ca07832bf169fb20c379897af71d09c97fb0.

--- 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
+++ 04-testing-configuration-logging/01-unit-tests/test/Validator.test.js
@@ -28,7 +28,7 @@ describe('testing-configuration-logging/unit-tests', () => {
           .to.have.property('error')
           .and.to.be.equal('too long, expect 20, got 60');
     });
-    it('Validator reports if not string type passed to string field', () => {
+    it('Validator reports if not string passed to string field', () => {
       const errors = this.validator.validate({
         name: 123123,
       });
@@ -61,7 +61,7 @@ describe('testing-configuration-logging/unit-tests', () => {
           .to.have.property('error')
           .and.to.be.equal('too big, expect 27, got 101');
     });
-    it('Validator reports if not number type passed to number field', () => {
+    it('Validator reports if not number passed to number field', () => {
       const errors = this.validator.validate({
         age: '123',
       });
@@ -71,34 +71,6 @@ describe('testing-configuration-logging/unit-tests', () => {
       expect(errors[0])
           .to.have.property('error')
           .and.to.be.equal('expect number, got string');
-    });
-    it('Validator reports if argument is not an object', () => {
-      const errors = this.validator.validate('TEST01');
-
-      expect(errors).to.have.length(1);
-      expect(errors[0])
-          .to.have.property('error')
-          .and.to.be.equal('expect argument type to be object, got string');
-    });
-    it('Validator does not report error if rules are respected', () => {
-      const errors = this.validator.validate({
-        age: 25,
-        name: 'John Kiedis',
-      });
-
-      expect(errors).to.have.length(0);
-    });
-    it('Validator does not report if no rules defined', () => {
-      const emptyValidator = new Validator();
-      const errors = emptyValidator.validate({afafaffa: 'asdasd'});
-
-      expect(errors).to.have.length(0);
-    });
-    it('Validator does not report if argument is an empty object', () => {
-      const emptyValidator = new Validator(validatorRules);
-      const errors = emptyValidator.validate({});
-
-      expect(errors).to.have.length(0);
     });
   });
 });
